d2a290e5f0713139b097d95fe6be6d051e7232eb
Hein Htet Zaw <devildog.kk@gmail.com>
2025-11-05 09:40:57 +0700

    f

--- 02_frontend/app/page.js
+++ 02_frontend/app/page.js
@@ -44,6 +44,7 @@ export default function Page() {
   return (
     <main className="container">
       <header className="header">
+        <h1 className="title">Hein Htet Zaw 6609533</h1>
         <h1 className="title">Attractions</h1>
         <p className="subtitle">Discover points of interest nearby</p>
       </header>
